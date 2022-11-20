6d2ce208757cf463fe3c7020fc7b168a3693314a
alinap7814 <alinap7814@gmail.com>
2022-11-20 11:32:53 +0200

    colorTurtleTraining

---
+++ colorTurtleTraining.py
@@ -0,0 +1,21 @@
+from turtle import bgcolor, circle, color, done, fd, rt, tracer, width
+
+def r(x, y):
+    rt(x)
+    fd(y)
+
+tracer(4)
+fd(100)
+bgcolor("black")
+color("cyan")
+width(3)
+
+for i in range(2003):
+    fd(i)
+    r(90, i)
+    r(90, i)
+    r(270, i)
+    r(90, i)
+    circle(100, 90)
+    
+done()
